07fd82e41d070fdce63cdc1409f446841fbf5003
Ole Martin Skaug <olemartin.skaug@gmail.com>
2015-01-17 13:58:53 +0100

    Commit tiney MC with image upload

--- nw-main-admin.php
+++ nw-main-admin.php
@@ -11,8 +11,15 @@ require('template/temp-header.php');
 
 	<!-- Main content -->
 	<section class="content">
+		<div class="col-md-6">
+		<textarea>Type here!</textarea>
+		<iframe id="form_target" name="form_target" style="display:none"></iframe>
+		<form id="my_form" action="/upload/" target="form_target" method="post" enctype="multipart/form-data" style="width:0px;height:0;overflow:hidden">
+		    <input name="image" type="file" onchange="$('#my_form').submit();this.value='';">
 
-
+		</form>	
+		</div>
+		<?php echo  $_SERVER['DOCUMENT_ROOT'];?>
 	</section><!-- /.content -->
 <?php
 require('template/temp-footer.php');
--- template/temp-footer.php
+++ template/temp-footer.php
@@ -10,6 +10,39 @@
         <script src="js/AdminLTE/app.js" type="text/javascript"></script>
         <!-- Scripts -->
         <script src="js/functions/signout.js"></script>
+        <!-- TinyMCE 4.x -->
+		<script type="text/javascript" src="core/editor/tinymce/tinymce.min.js"></script>
+		<script type="text/javascript">
+		 
+		tinymce.init({
+		  selector: "textarea",
+		  
+		  // ===========================================
+		  // INCLUDE THE PLUGIN
+		  // ===========================================
+			
+		  plugins: [
+		    "advlist autolink lists link image charmap print preview anchor",
+		    "searchreplace visualblocks code fullscreen",
+		    "insertdatetime media table contextmenu paste jbimages"
+		  ],
+			
+		  // ===========================================
+		  // PUT PLUGIN'S BUTTON on the toolbar
+		  // ===========================================
+			
+		  toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image jbimages",
+			
+		  // ===========================================
+		  // SET RELATIVE_URLS to FALSE (This is required for images to display properly)
+		  // ===========================================
+			
+		  relative_urls: false
+			
+		});
+		 
+		</script>
+		<!-- /TinyMCE -->
 
     </body>
 </html>
